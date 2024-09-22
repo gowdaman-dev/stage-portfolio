2213018d9f2e0c3e8a1b64d3be0a382d6c9ff199
gowdaman-dev <damangowdaman@gmail.com>
2024-09-22 12:02:32 +0530

    new

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+//portfolio home page
+function Home() {
+  return (
+    <section>
+        <h1>Welcome to my portfolio!</h1>
+        <p>Take a look around and learn more about me.</p>
+    </section>
+  )
+}
+
+export default Home
